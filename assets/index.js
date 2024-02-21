c997d19807cef8c27eabb809b2a945132a3b95c4
AbUtEcH bRo <idrisabubakry@gmail.com>
2024-02-21 11:36:56 +0100

    picki0 commit

--- assets/index.js
+++ assets/index.js
@@ -137,18 +137,10 @@ function clearAll() {
   
   // source: https://www.goodreads.com/quotes/tag/programming
   const quotes = [
-    { quote: 'Programs must be written for people to read, and only incidentally for machines to execute.', author: 'Harold Abelson'},
-    { quote: 'Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.', author: 'Rick Cook' },
-    { quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live', author: 'John Woods' },
-    { quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', author: 'Martin Fowler' },
-    { quote: 'Truth can only be found in one place: the code.', author: 'Robert C. Martin' },
-    { quote: 'That\'s the thing about people who think they hate computers. What they really hate is lousy programmers.', author: 'Larry Niven' },
-    { quote: 'You\'ve baked a really lovely cake, but then you\'ve used dog shit for frosting.', author: 'Steve Jobs' },
-    { quote: 'A language that doesn\'t affect the way you think about programming is not worth knowing.', author: 'Alan J. Perlis' },
-    { quote: 'The most disastrous thing that you can ever learn is your first programming language.', author: 'Alan Kay' },
-    { quote: 'The computer programmer is a creator of universes for which he alone is the lawgiver. No playwright, no stage director, no emperor, however powerful, has ever exercised such absolute authority to arrange a stage or field of battle and to command such unswervingly dutiful actors or troops.', author: 'Joseph Weizenbaum' },
-    { quote: 'Everyone knows that debugging is twice as hard as writing a program in the first place. So if you\'re as clever as you can be when you write it, how will you ever debug it?', author: 'Brian Kernighan' },
-    { quote: 'No matter which field of work you want to go in, it is of great importance to learn at least one programming language.', author: 'Ram Ray' },
+    { quote: 'Are you Aisha? Because you are enteringmy eyes sha😎 '},
+    { quote: 'We are asked to lower our gaze but when I look down I found my kids jannah at feet' },
+    { quote: 'Are you Isha? because my day does not seems to end without you.' },
+    { quote: 'Is your heart wudu, Because I never want to break it' },
   ];
   
   // Source: https://www.rd.com/list/challenging-riddles/
